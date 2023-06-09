fa032327bf80635a24a7191fa55b64bebd5b15f9
Kiran Khairnar <kkhairnar@cpex.com>
2023-06-09 13:42:06 +0530

    clean repo

--- data1.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd 
-from pandas_profiling import ProfileReport  
-df = pd.read_csv('Target_Table_Snapshot.csv')
-profile = ProfileReport(df, title="Pandas Profiling Report")
-profile.to_file('EDA_snapshot_Analysis.html')
